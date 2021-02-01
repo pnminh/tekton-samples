f78d397ce8d049f49e5e860771eb9142d52f4d10
pnminh <pnminh232@gmail.com>
2021-02-01 13:49:57 -0600

    add spring project

---
+++ simple-with-inputs-and-params/tekton-spring-sample/src/main/java/com/minh/tekton/sample/tektonspringsample/TektonSpringSampleApplication.java
@@ -0,0 +1,13 @@
+package com.minh.tekton.sample.tektonspringsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TektonSpringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TektonSpringSampleApplication.class, args);
+	}
+
+}
---
+++ simple-with-inputs-and-params/tekton-spring-sample/src/test/java/com/minh/tekton/sample/tektonspringsample/TektonSpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.tekton.sample.tektonspringsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TektonSpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
